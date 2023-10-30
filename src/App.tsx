312bc327d7a6e9765e153d5f4454f87a935f6dd8
Mohammed Askar <mdaskar40@gmail.com>
2023-10-30 14:48:36 +0530

    trial

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ function App() {
   return (
     <div className="App">
       <div>
+        <p>hola amigo</p>
+      </div>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
