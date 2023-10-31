95ec61e240abcf9e32a349fb7e159fceb55ca16c
Mohammed Askar <mdaskar40@gmail.com>
2023-10-31 22:58:42 +0530

    add sidebar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+import Sidebar from "./sidebar/sidebar";
+
+const navbar = () => {
+  return (
+    <>
+      <div className="nav ">
+        <Sidebar />
+        <div className="nav_wrapper">
+          <motion.span
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            Mohammed Askar
+          </motion.span>
+          <div className="social">
+            <a
+              href="https://www.facebook.com/profile.php?id=100007086048806"
+              target="_blank"
+            >
+              <img src="/facebook.png" />
+            </a>
+            <a href="https://www.instagram.com/buri_buri___/" target="_blank">
+              <img src="/instagram.png" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mohammed-askar-2aa92725a"
+              target="_blank"
+            >
+              <img src="/youtube.png" />
+            </a>
+            <a href="https://github.com/Askar1199" target="_blank">
+              <img src="/dribbble.png" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default navbar;
---
+++ src/components/sidebar/sidebar.tsx
@@ -0,0 +1,73 @@
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+const sidebar = () => {
+  const [btn, setBtn] = useState(false);
+  const items =["Home","Services","Portfolio","Contact","About"]
+
+  const click = () => {
+    setBtn((current) => !current);
+  };
+
+  const varients = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20,
+      },
+    },
+    closed: {
+      clipPath: "circle(20px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40,
+      },
+    },
+  };
+  return (
+    <>
+      <motion.div className="sidebar" animate={btn ? "open" : "closed"}>
+        <motion.div className="bg" variants={varients}>
+          <div className="links">{items.map((map,ind)=>(
+            <a href={`${map}`} key={ind}>{map}</a>
+          ))}</div>
+        </motion.div>
+
+        <button className="buttons" onClick={click}>
+          <svg width="23" height="23" viewBox="0 0 23 23">
+            <motion.path
+              strokeWidth="3"
+              stroke="black"
+              strokeLinecap="round"
+              variants={{
+                closed: { d: "M 2 2.5 L 20 2.5" },
+                open: { d: "M 3 16.5 L 17 2.5" },
+              }}
+            />
+            <motion.path
+              strokeWidth="3"
+              stroke="black"
+              strokeLinecap="round"
+              d="M 2 9.423 L 20 9.423"
+              variants={{ closed: { opacity: 1 }, open: { opacity: 0 } }}
+            />
+            <motion.path
+              strokeWidth="3"
+              stroke="black"
+              strokeLinecap="round"
+              variants={{
+                closed: { d: "M 2 16.346 L 20 16.346" },
+                open: { d: "M 3 2.5 L 17 16.346" },
+              }}
+            />
+          </svg>
+        </button>
+      </motion.div>
+    </>
+  );
+};
+
+export default sidebar;
