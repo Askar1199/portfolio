ed36ee948c2d1af6795c613d52bd7c9f46857093
Mohammed Askar <mdaskar40@gmail.com>
2023-11-05 15:26:25 +0530

    changed images public

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,6 @@
 import { motion } from "framer-motion";
 import Sidebar from "./sidebar/sidebar";
-import fb from "../assets/facebook.png";
-import ins from "../assets/instagram.png";
-import git from "../assets/github.png";
-import lin from "../assets/linkedin.png";
+
 
 const navbar = () => {
   return (
@@ -23,19 +20,19 @@ const navbar = () => {
               href="https://www.facebook.com/profile.php?id=100007086048806"
               target="_blank"
             >
-              <img src={fb} />
+              <img src="./facebook.png" />
             </a>
             <a href="https://www.instagram.com/buri_buri___/" target="_blank">
-              <img src={ins} />
+              <img src="./instagram.png" />
             </a>
             <a
               href="https://www.linkedin.com/in/mohammed-askar-2aa92725a"
               target="_blank"
             >
-              <img src={lin} />
+              <img src="./linkedin.png" />
             </a>
             <a href="https://github.com/Askar1199" target="_blank">
-              <img src={git} />
+              <img src="./github.png" />
             </a>
           </div>
         </div>
